4490c00b03529182014037e5ea34c4af1ce50af3
osmarruiz <osmarernesto17@gmail.com>
2025-07-17 01:25:16 -0600

    port solution

--- testdeploy/Program.cs
+++ testdeploy/Program.cs
@@ -3,6 +3,13 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.WebHost.ConfigureKestrel(serverOptions =>
+{
+    // Obtiene el puerto de la variable de entorno PORT, o usa 8080 como fallback
+    var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+    serverOptions.ListenAnyIP(int.Parse(port)); // Escucha en cualquier IP en el puerto especificado
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
